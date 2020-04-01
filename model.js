6846d47e06d82f4ee409ca56867e4b6dd7faf56a
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-04-01 18:53:45 +0100

    add schema and prepare connect.js

--- model.js
+++ model.js
@@ -1,9 +1,9 @@
- let counter = 0;
- let posts = {}
+let counter = 0;
+let posts = {}
 function increment() {
   counter++
 }
 function getCount() {
   return counter
 }
- module.exports = {getCount, increment, posts };
+module.exports = {getCount, increment, posts };
